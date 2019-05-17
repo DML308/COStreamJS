996356d379e450352b803468fde2e9bb5e2968f8
lxx.imac <lxx2013@mail.ustc.edu.cn>
2019-05-17 17:11:05 +0800

    feat 增加一些节点的 toString 方法,为了代码生成时调用 && ast2ssg 时展开节点时用来调试

--- src/ast/toString.js
+++ src/ast/toString.js
@@ -1,20 +1,80 @@
-import { jump_statement, blockNode, expNode, labeled_statement, forNode } from "./node.js"
-import { error } from "../utils"
+import { jump_statement, blockNode, expNode, labeled_statement, forNode, declareNode, declarator, compositeNode, ComInOutNode, compBodyNode, inOutdeclNode, strdclNode, paramNode, parameter_declaration, binopNode, operatorNode, operBodyNode, arrayNode, constantNode, unaryNode } from "./node.js"
+
+export function ast2String(root) {
+    var result = ''
+    root.forEach((x, idx) => {
+        result += x.toString() + (x instanceof declareNode ? ';' : '') + '\n'
+    })
+    return result.replace(/ {2,}/g, ' ').beautify()
+}
+/**
+ * 输入一个 list 返回它转化后的 string, 可以配置分隔符 split 例如','
+ * 也可以配置 start 和 end 例如'{' '}'
+ */
+function list2String(list, split, start, end) {
+    if (!list || list.length == 0) return ''
+    var str = start ? start : ''
+    list.forEach((x, idx) => {
+        str += x.toString()
+        str += split && idx < list.length - 1 ? split : ''
+    })
+    return end ? str + end : str
+}
 
 /**
-* 加载toString插件,加载该插件后, statement 类型的节点可以执行 toString 用于代码生成
+* 加载toString插件,加载该插件后, statement 类型的节点可以执行 toString 用于代码生成或调试
 */
 export function loadToStringPlugin() {
+    declarator.prototype.toString = function () {
+        var str = this.identifier.toString() + ' '
+        str += this.op1 ? this.op1 : ''
+        str += this.parameter ? this.parameter.toString() : ''
+        str += this.op2 ? this.op2 : ''
+        if (this.initializer instanceof Array) {
+            str += list2String(this.initializer, ',', '{', '}')
+        } else {
+            str += this.initializer ? this.initializer.toString() : ''
+        }
+        return str
+    }
+    declareNode.prototype.toString = function () {
+        return this.type + ' ' + list2String(this.init_declarator_list, ',')
+    }
+    compositeNode.prototype.toString = function () {
+        var str = 'composite' + this.compName + '('
+        str += this.inout ? this.inout.toString() : ''
+        str += ')' + this.body.toString()
+        return str
+    }
+    ComInOutNode.prototype.toString = function () {
+        return 'input ' + list2String(this.input_list) + ', ouput ' + list2String(this.output_list)
+    }
+    inOutdeclNode.prototype.toString = function () {
+        return this.strType.toString() + this.id
+    }
+    strdclNode.prototype.toString = function () {
+        var str = 'stream<'
+        this.id_list.forEach(({ type, identifier }) => {
+            str += type + ' ' + identifier + ','
+        })
+        return str.slice(0, -1) + '>'
+    }
+    compBodyNode.prototype.toString = function () {
+        var str = '{\n'
+        str += this.param ? this.param.toString() : ''
+        str += list2String(this.stmt_list, ';\n') + '\n}\n'
+        return str
+    }
+    paramNode.prototype.toString = function () {
+        return 'param\n  ' + list2String(this.param_list, ',')
+    }
+    parameter_declaration.prototype.toString = function () {
+        return this.type + ' ' + this.declarator.toString()
+    }
     //将每一行 statement 的';'上提至 blockNode 处理
     blockNode.prototype.toString = function () {
-        var str = '{';
-        this.stmt_list && this.stmt_list.forEach((node) => {
-            str += node.toString()
-            if ([expNode].some(x => node instanceof x)) {
-                str += ';'
-            }
-            str += '\n'
-        })
+        var str = '{\n';
+        str += list2String(this.stmt_list, ';\n')
         return str + '}\n'
     }
     jump_statement.prototype.toString = function () {
@@ -27,9 +87,28 @@ export function loadToStringPlugin() {
         str += this.op2 ? this.op2 : ''
         return str + ' ' + this.op3 + this.statement.toString()
     }
-    //TODO:未完成,待填充
-    expNode.prototype.toString = function () {
+    //expNode 的子类
+    binopNode.prototype.toString = function () {
+        return this.left + this.op + this.right
+    }
+    arrayNode.prototype.toString = function () {
+        return '' + this.exp + list2String(this.arg_list, '][', '[', ']')
+    }
+    constantNode.prototype.toString = function () {
         return this.value
+    }
+    unaryNode.prototype.toString = function () {
+        return '' + this.first + this.second
+    }
+    operatorNode.prototype.toString = function () {
+        return this.operName + '(' + this.inputs + ')' + this.operBody
+    }
+    operBodyNode.prototype.toString = function () {
+        var str = list2String(this.stmt_list, ';\n') + '{\n'
+        str += this.init ? 'init' + this.init : ''
+        str += this.work ? 'work' + this.work : ''
+        str += this.win ? 'window' + this.win : ''
+        return str + '\n}\n'
     }
     forNode.prototype.toString = function () {
         var str = 'for('
