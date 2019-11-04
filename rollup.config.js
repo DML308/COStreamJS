f5494baf3f5e65eb8df29b7f6f1594495e546798
lxx.imac <lxx2013@mail.ustc.edu.cn>
2019-11-04 14:42:41 +0800

    config: 修改 rollup 配置, 输出浏览器用的和 cli 双版本

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,20 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 export default {
-    input: 'main.js',
-    output: {
-        file: 'dist/COStreamJS.js',
-        format: 'iife',
-        name: 'COStreamJS'
-    },
-    plugins: [
-        resolve(),
-        commonjs(),
-    ],
-    external: ['fs']
-}  
+	input: 'main.js',
+	output: [
+		{
+			file: 'dist/COStreamJS.js',
+			format: 'iife',
+			name: 'COStreamJS'
+		},
+		{
+			file: 'dist/costreamjs-cli.js',
+			format: 'iife',
+            name: 'COStreamJS',
+            banner:'#!/usr/bin/env node'
+		}
+	],
+	plugins: [ resolve(), commonjs() ],
+	external: [ 'fs' ]
+};
