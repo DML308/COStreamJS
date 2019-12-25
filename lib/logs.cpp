d718ad947742ca313e04646a87d6d4e2f2242437
lxx.mbp2015 <lxx2013@mail.ustc.edu.cn>
2019-12-26 00:11:06 +0800

    feat: 在 logs 中实现 print 和 println 函数

--- lib/logs.cpp
+++ lib/logs.cpp
@@ -1,31 +1,5 @@
-#include<iostream>
-#include <sstream>
-
-using namespace std;
-
-// 参考链接 https://elloop.github.io/c++/2015-11-28/never-proficient-cpp-vaargs
-// 函数模板递归出口
-template <typename T>
-string print(T t) {
-    stringstream s;
-    s << t;
-    return s.str(); 
-}
-
-template <typename T, typename ... Args>
-string print(T t, Args... args) {
-    stringstream ss;
-    string s;
-    ss << t;
-    s = ss.str() + print(args...);  
-    return s;
-}
-
-
-
-
-
-int main(){
-    stringstream s,s2;
-    cout<< print(1,2.2, " 345","6\n","7");
-}
+#include "logs.h"
+#ifdef DEBUG
+int logs_length = 0;
+string logs[100];
+#endif
