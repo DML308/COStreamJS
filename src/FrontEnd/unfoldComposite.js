91dd83b62a73cf703ceed4c4dcdb25514e7c0dfd
lxx.mbp2015 <lxx2013@mail.ustc.edu.cn>
2020-03-24 23:04:34 +0800

    refactor: 将GraphToOperators 拆分为2段遍历, 将unfold 展开后的 comp 插入到 body 中以 Out = comp(In); 的形式

--- src/FrontEnd/unfoldComposite.js
+++ src/FrontEnd/unfoldComposite.js
@@ -1,8 +1,7 @@
 import { deepCloneWithoutCircle } from "../utils"
 import { COStreamJS } from "./global"
 import { addNode, parenNode, forNode, compositeCallNode, splitjoinNode, pipelineNode, ComInOutNode, compHeadNode, compBodyNode, compositeNode, binopNode, operatorNode, splitNode, roundrobinNode, duplicateNode, joinNode, constantNode, blockNode, declareNode, operBodyNode, winStmtNode, declarator, idNode, inOutdeclNode, strdclNode, unaryNode } from "../ast/node";
-import { matrix_section } from "../ast/node";
-import { matrix_slice_pair } from "../ast/node";
+import { matrix_section, matrix_slice_pair, layerNode } from "../ast/node";
 import { top, setTop } from "./generateSymbolTables"
 import { SymbolTable } from "./symbol";
 
--- src/LifeCycle/ast2ssg.js
+++ src/LifeCycle/ast2ssg.js
@@ -2,7 +2,7 @@ import { StaticStreamGraph } from "../FrontEnd/StaticStreamGraph";
 import { top, setTop, generateCompositeRunningContext } from "../FrontEnd/generateSymbolTables" 
 import { runningStack, SymbolTable, Variable } from "../FrontEnd/symbol"
 import { debug } from "../utils"
-import { binopNode, operatorNode, compositeCallNode, splitjoinNode, pipelineNode, operNode, sequentialNode } from "../ast/node";
+import { binopNode, operatorNode, compositeCallNode, splitjoinNode, pipelineNode, operNode, sequentialNode, parenNode } from "../ast/node";
 
 /*
  *  功能：将抽象语法树转为平面图
@@ -32,16 +32,13 @@ export function AST2FlatStaticStreamGraph(mainComposite,unfold,S){
     return ssg
 }
 
-/*
-* 功能：递归的调用，
-* 完成splitjoin和pipeline节点的展开，以及完成opearatorNode到flatnode节点的映射
-* 输入参数：composite
-* 输出：设置静态数据流图的对应flatNode节点，完成数据流边到flatNode节点的映射
-*/
 
 /**
- * 1.遇到 out = call(in){ int / work / window } 形式的 operatorNode, 则在 ssg 中创建该 flatNode 并连接Edge
- * 2.遇到 pipeline 或 splitjoin , 则将其展开为一个真正的 composite 并挂载至 COStream.ast
+ * 功能：递归的调用，生成 ssg 中的所有 flatNode
+ * 算法思路: 2遍遍历
+ * 1. 在第一遍遍历中 遇到 pipeline 或 splitjoin 等复合结构, 则将其展开为一个真正的 composite 并挂载至 COStream.ast
+ * 2. 在第二遍遍历中 遇到 out = call(in){ int / work / window } 形式的 operatorNode, 则在 ssg 中创建该 flatNode 并连接Edge
+ *                 遇到 out = compCall(in); 形式的 compositeCall 调用, 则查符号表找到该 composite, 再深入其中来连接数据流
  * 
  * @param {operNode} call
  * @param {compositeNode} composite
@@ -54,9 +51,38 @@ function GraphToOperators(call, composite, ssg, unfold, S, params = []){
     runningStack.push(top)
     generateCompositeRunningContext(call, composite, params); //传入参数,并生成 composite 调用的执行上下文环境
 
+
+    /** 先执行第一遍遍历, 检查是否有符合解构, 若有则展开复合结构, 将其替换为传统 compositeCall 调用, 方便打断点 toString 调试
+     * 例如将  Out = splitjoin(In){ ... }; 替换为 Out = splitjoin_0(In);  在 ast 中会出现额外的定义 composite splitjoin_0(...) { ... }
+     * 例如将  Out = sequential(In){ ... }; 替换为 Out = sequential(In);  在 ast 中会出现额外的定义 composite sequential(...) { ... }
+     */
+    for(let i in composite.body.stmt_list){
+
+        let it = composite.body.stmt_list[i], call;
+        let exp = it instanceof binopNode ? it.right : it //获取到要处理的 operNode, 无论是直接调用还是通过 binopNode 的 right 来调用
+
+        switch(exp.constructor){
+            case splitjoinNode: call = unfold.UnfoldSplitJoin(exp); break;
+            case pipelineNode: call = unfold.UnfoldPipeline(exp); break;
+            case sequentialNode: call = unfold.UnfoldSequential(exp); break;
+            default: continue;
+        }
+
+        if(call.outputs && call.outputs.length > 0){
+            const left = call.outputs.length > 1? new parenNode(it._loc, call.outputs) : call.outputs[0]
+            const binop = new binopNode(it._loc, left,'=', call)
+            composite.body.stmt_list[i] = binop
+        }else{
+            composite.body.stmt_list[i] = call
+        }   
+        
+    }
+    // 展开完毕, 可在 chrome 控制台中输入右侧代码来查看展开的结果 console.log(COStreamJS.ast.map(_=>_+'').join('\n').beautify())
+
+    /** 再执行第二遍遍历, 这次遍历中, 仅有 operator 或 compositCall */
     for (let it of composite.body.stmt_list){
         
-        let exp = it instanceof binopNode ? it.right : it //获取到要处理的 operator(){}或 pipeline()或其他,无论是直接调用还是通过 binopNode 的 right 来调用
+        let exp = it instanceof binopNode ? it.right : it
 
         if(exp instanceof operatorNode){
             ssg.GenerateFlatNodes(exp, params)
@@ -67,20 +93,6 @@ function GraphToOperators(call, composite, ssg, unfold, S, params = []){
             
             GraphToOperators(exp,actual_composite, ssg, unfold,S,params);
             
-        }else if(exp instanceof splitjoinNode){
-            const call = unfold.UnfoldSplitJoin(exp)
-            const actual_composite = S.compTable[call.compName].composite // 查看该生成的结构: debug(actual_composite.toString().beautify())
-            GraphToOperators(call, actual_composite, ssg, unfold,S)
-
-        }else if(exp instanceof pipelineNode){
-            const call = unfold.UnfoldPipeline(exp)
-            const actual_composite = S.compTable[call.compName].composite
-            GraphToOperators(call, actual_composite, ssg, unfold, S)
-
-        }else if(exp instanceof sequentialNode){
-            const call = unfold.UnfoldSequential(exp)
-            const actual_composite = S.compTable[call.compName].composite
-            GraphToOperators(call, actual_composite, ssg, unfold, S)
         }
     }
 
