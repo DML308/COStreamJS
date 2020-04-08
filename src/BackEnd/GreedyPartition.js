9ddbdfd6721b994635fe8443524b53581dc78161
lxx.mbp2015 <lxx2013@mail.ustc.edu.cn>
2020-04-09 00:41:24 +0800

    fix: 修复了一个错误,该错误会导致核号-flatNodes 的映射结果乱序

--- src/BackEnd/GreedyPartition.js
+++ src/BackEnd/GreedyPartition.js
@@ -71,7 +71,11 @@ GreedyPartition.prototype.SssgPartition = function (ssg) {
         flatNodes.forEach(flat => {
             this.FlatNode2PartitionNum.set(flat, coreNum)
         })
-        this.PartitonNum2FlatNode.set(coreNum, flatNodes)
+        // 设置划分核号 - flatNode 节点的映射时, 对其按标号排序
+        this.PartitonNum2FlatNode.set(coreNum, flatNodes.sort((a,b)=>{
+            let a_num = a.name.split('_').pop(), b_num = b.name.split('_').pop()
+            return a_num - b_num
+        }))
     })
 }
 /**
